2fba9df4611876dc55f561d9638fcf6954ab5922
caeltarifa <caeltarifa@gmail.com>
2021-03-17 12:12:33 -0400

    Update control_automatico_pib.sql
    aCTUALIZANDO EL TRIGGER CON CONTROL AUTOMATICO DEL PIB FUNCTION

--- control_automatico_pib.sql
+++ control_automatico_pib.sql
@@ -4,7 +4,34 @@
 3 si fuera NOTAMC, actualiza es_pib=false de la tabla charly_cancel
 
 ''' --------------------------------------------------------------FUNCTION TRIGGER -----------------------------------------------------------------------------------------------'''
-
+create or replace function control_automatico_pib() returns trigger as $control_automatico_pib$
+declare
+	reg RECORD;
+	cur_notam CURSOR FOR SELECT distinct tn.id_notam_pib FROM
+	(select id_notam_pib, split_part(id_notam_pib,' ',3)as codigo from plan_vuelo_pib_tiempo_real
+	where (id_notam_pib like '%NOTAMR%') OR (id_notam_pib like '%NOTAMC%')
+	)as t1
+	inner join
+	plan_vuelo_pib_tiempo_real as tn
+	on
+	tn.id_notam_pib like '(' || t1.codigo ||'%';
+	
+	BEGIN
+	for reg in cur_notam 
+	LOOP
+		if reg is null then
+		else
+			delete from plan_vuelo_pib_tiempo_real where id_notam_pib like reg.id_notam_pib;
+			update plan_vuelo_notam_trafico_charly_new set es_pib = 'false'
+			where idnotam like reg.id_notam_pib || '%';
+			update plan_vuelo_notam_trafico_charly_repla set es_pib = 'false' 
+			where idnotam like '%' || reg.id_notam_pib;
+		end if;
+		--raise notice 'Procesando %', reg.id_notam_pib; 
+	END LOOP;
+	return NEW;
+end;
+$control_automatico_pib$ LANGUAGE 'plpgsql';
 ''' --------------------------------------------------------------TRIGGER AFTER INSERT en pib_tiempo_real, verifica si es CANCEL o REPLACE y actualiza el PIB -----------------------------------------------------------------------------------------------'''
 DROP TRIGGER IF EXISTS control_automatico_pib 
 ON plan_vuelo_pib_tiempo_real  CASCADE  ;
