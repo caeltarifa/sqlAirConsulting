699cd8de638c640f65238eea0598df699e1fd60d
caeltarifa <caeltarifa@gmail.com>
2021-03-27 11:45:51 -0400

    Update control_automatico_pib.sql
    Funcion definitiva de validacion de datos en el PIB TIEMPO REAL

--- control_automatico_pib.sql
+++ control_automatico_pib.sql
@@ -4,37 +4,49 @@
 3 si fuera NOTAMC, actualiza es_pib=false de la tabla charly_cancel
 
 ''' --------------------------------------------------------------FUNCTION TRIGGER -----------------------------------------------------------------------------------------------'''
-create or replace function control_automatico_pib() returns trigger as $control_automatico_pib$
-declare
-	reg RECORD;
-	cur_notam CURSOR FOR SELECT distinct tn.id_notam_pib FROM
-	(select id_notam_pib, split_part(id_notam_pib,' ',3)as codigo from plan_vuelo_pib_tiempo_real
-	where (id_notam_pib like '%NOTAMR%') OR (id_notam_pib like '%NOTAMC%')
-	)as t1
-	inner join
-	plan_vuelo_pib_tiempo_real as tn
-	on
-	tn.id_notam_pib like '(' || t1.codigo ||'%';
-	
+CREATE or replace FUNCTION public.control_automatico_pib()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+AS $BODY$
+	declare
+		reg varchar;
+		vector_notam_enpib text[];
+		sw boolean;
 	BEGIN
-	for reg in cur_notam 
-	LOOP
-		if reg is null then
-		else
-			delete from plan_vuelo_pib_tiempo_real where id_notam_pib like reg.id_notam_pib;
-			update plan_vuelo_notam_trafico_charly_new set es_pib = 'false'
-			where idnotam like reg.id_notam_pib || '%';
-			update plan_vuelo_notam_trafico_charly_repla set es_pib = 'false' 
-			where idnotam like '%' || reg.id_notam_pib;
-		end if;
-		--raise notice 'Procesando %', reg.id_notam_pib; 
-	END LOOP;
+		sw=false;
+		vector_notam_enpib := (select ARRAY(SELECT distinct tn.id_notam_pib FROM
+		(select id_notam_pib, split_part(id_notam_pib,' ',3)as codigo from plan_vuelo_pib_tiempo_real
+		where (id_notam_pib like '%NOTAMR%') OR (id_notam_pib like '%NOTAMC%')
+		)as t1
+		inner join
+		plan_vuelo_pib_tiempo_real as tn
+		on
+		tn.id_notam_pib like '(' || t1.codigo ||'%'));
+		
+		foreach reg in array vector_notam_enpib
+--		for reg in cur_notamy 
+			LOOP
+				if reg is null then
+				else
+					raise notice ' var ... %', reg;
+					delete from plan_vuelo_pib_tiempo_real where id_notam_pib like reg || '%';
+					update plan_vuelo_notam_trafico_charly_new set es_pib = 'false'
+					where idnotam like reg || '%';
+					update plan_vuelo_notam_trafico_charly_repla set es_pib = 'false' 
+					where idnotam like '%' || reg;
+					sw=true;
+				end if;
+				--raise notice 'Procesando %', reg.id_notam_pib; 
+			END LOOP;
 	
 	--ACTUALIZAMOS EL DOCUMENTO DE REGISTRO PIB EN LA BASE DE DATOS, REGISTRANDO LA ULTIMA MODIFICACION
-	insert into aro_ais_historico_pib (lista_notam, fecha_modificado) values ( documentacion_notam_pib(), current_timestamp);
+	if sw then 
+		insert into aro_ais_historico_pib (lista_notam, fecha_modificado) values ( documentacion_notam_pib(), current_timestamp);
+	end if;
+	
 	return NEW;
 end;
-$control_automatico_pib$ LANGUAGE 'plpgsql';
+$BODY$;
 
 ''' --------------------------------------------------------------TRIGGER AFTER INSERT en pib_tiempo_real, verifica si es CANCEL o REPLACE y actualiza el PIB -----------------------------------------------------------------------------------------------'''
 DROP TRIGGER IF EXISTS control_automatico_pib 
